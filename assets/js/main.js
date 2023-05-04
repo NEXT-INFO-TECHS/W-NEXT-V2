93c6a10f20bcfb37895076357c856776e24b5e85
Monoget <monoget2@gmail.com>
2023-05-04 13:18:57 +0600

    update home

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,3 +102,26 @@ $(document).ready(function () {
     });
 });
 
+let countDownDate = new Date("Jul 25, 2023 16:37:52").getTime();
+
+let myfunc = setInterval(function() {
+    let now = new Date().getTime();
+    let timeleft = countDownDate - now;
+
+    let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+    document.getElementById("hours").innerHTML = hours + "h"
+    document.getElementById("mins").innerHTML = minutes + "m"
+    document.getElementById("secs").innerHTML = seconds + "s"
+
+    if (timeleft < 0) {
+        clearInterval(myfunc);
+        document.getElementById("hours").innerHTML = ""
+        document.getElementById("mins").innerHTML = ""
+        document.getElementById("secs").innerHTML = ""
+    }
+}, 1000)
+
